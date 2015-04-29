f7db164c9d32807d865cde876806a4c4947f8a10
Stephen Smith <sns1215@gmail.com>
2015-04-29 15:21:08 -0400

    Added closer functionality to what it will be

--- src/stepWizard.js
+++ src/stepWizard.js
@@ -45,11 +45,62 @@ var StepWizard = React.createClass({
     }
   },
 
+  onClickNext: function() {
+    var stepIndex = this.state.currentStepIndex;
+    var maxIndex = React.Children.count(this.props.children) - 1;
+
+    stepIndex = Math.min(stepIndex + 1, maxIndex);
+    
+    this.moveToPage(stepIndex);
+  },
+
+  onClickPrev: function() {
+    var stepIndex = this.state.currentStepIndex;
+
+    stepIndex = Math.max(stepIndex - 1, 0);
+    
+    this.moveToPage(stepIndex);
+  },
+
+  moveToPage: function(stepIndex) {
+    var minIndex = 0;
+    var maxIndex = React.Children.count(this.props.children) - 1;
+
+    if(stepIndex > maxIndex) {
+      console.warn("stepIndex was " + stepIndex + ", greater than maxIndex of " + maxIndex + ". Setting stepIndex to " + maxIndex + ".");
+      stepIndex = maxIndex;
+    } else if(stepIndex < minIndex) {
+      console.warn("stepIndex was " + stepIndex + ", greater than minIndex of " + minIndex + ". Setting stepIndex to " + minIndex + ".");
+      stepIndex = minIndex;
+    }
+
+    this.setState({currentStepIndex: stepIndex});
+  },
+
+  getBeads: function() {
+    var beads = this.props.children.map(function(child, index) {
+      if(index === this.state.currentStepIndex) {
+        return "X";
+      }
+
+      return "O";
+    }, this);
+
+    return (<div className="sw-beads">{beads.join("---")}</div>);
+  },
 
   render: function () {
+    var beads = this.getBeads();
+    var currentStep = this.props.children[this.state.currentStepIndex];
+
     return (
       <div className="sw-container">
-        {this.props.children}
+        {beads}
+        {currentStep}
+        <div className="sw-navigation">
+          <input type="button" value="Previous" onClick={this.onClickPrev}/>
+          <input type="button" value="Next" onClick={this.onClickNext}/>
+        </div>
       </div>
     );
   },
